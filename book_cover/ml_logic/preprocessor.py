220e64adae3bef91216f8de9353a8aa52839cade
Nahia Peschard <nahia.peschard@outlook.fr>
2023-06-13 15:00:03 +0200

    Finalised and tested the preprocess_images function from preprocessor.py

--- book_cover/ml_logic/preprocessor.py
+++ book_cover/ml_logic/preprocessor.py
@@ -4,14 +4,25 @@ from PIL import Image
 import os
 
 
-def preprocess_images(X: pd.DataFrame)-> np.ndarray:
+def preprocess_images(X: pd.DataFrame, resize_size: tuple)-> np.ndarray:
+    """
+    INPUT
+    X: the DataFrame from which we want to retrieve the image names in the 'Image_name' column
+
+    resize_size: a tuple to which dimensions the images should be resized
+    For example (100, 100) resizes the images to 100 x 100 pixels
+
+    OUTPUT
+    A numpy array of shape (number of images, pixel length, pixel height, color channels)
+    """
+
+    output = []
 
     for image_name in X['Image_name']:
         image_path = os.path.join("../raw_data/Images/", image_name)
         image = Image.open(image_path)
-        image = image.resize((100, 100)) # Resizing to 100x100 pixels for speed purposes
+        image = image.resize(resize_size) # Resizing for speed purposes
         image_array = np.array(image)
+        output.append(image_array)
 
-        flattened_pixels = image_array.reshape(-1, 3)
-
-    return flattened_pixels
+    return np.array(output)
