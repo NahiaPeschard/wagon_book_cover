9590aca8530bc842e63ca899a56967db0455da42
Nahia Peschard <nahia.peschard@outlook.fr>
2023-06-13 14:05:25 +0200

    Nothing new, just want to change branch

--- book_cover/ml_logic/preprocessor.py
+++ book_cover/ml_logic/preprocessor.py
@@ -9,7 +9,7 @@ def preprocess_images(X: pd.DataFrame)-> np.ndarray:
     for image_name in X['Image_name']:
         image_path = os.path.join("../raw_data/Images/", image_name)
         image = Image.open(image_path)
-        image = image.resize((100, 100))
+        image = image.resize((100, 100)) # Resizing to 100x100 pixels for speed purposes
         image_array = np.array(image)
 
         flattened_pixels = image_array.reshape(-1, 3)
